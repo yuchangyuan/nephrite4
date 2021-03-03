5b16611964c31c07b454c02e6c6e9e558f7623d1
Yu Changyuan <reivzy@gmail.com>
2021-03-03 20:31:39 +0800

    nep-index update for Store::walk

--- query/src/bin/nep-index.rs
+++ query/src/bin/nep-index.rs
@@ -1,8 +1,6 @@
 // nephrite-index
 
 use nephrite4_common::conf;
-
-use nephrite4_query::index;
 use nephrite4_common::store::Store;
 
 use hex;
@@ -16,16 +14,17 @@ fn main() -> Result<()> {
     env_logger::init();
 
     let conf = conf::Conf::read();
-    let indexer = index::Indexer::new(&conf)?;
+    //let indexer = index::Indexer::new(&conf)?;
+    let store = Store::new(&conf)?;
 
-    let inc_ref = indexer.store.show_ref(Store::INC_REF)?.unwrap();
+    let inc_ref = store.show_ref(Store::INC_REF)?.unwrap();
 
-    let list = indexer.walk(&inc_ref, &None)?;
+    let list = store.walk(&inc_ref, &None)?;
 
     for (commit, tree) in list {
         println!("{}: {}", hex::encode(commit), hex::encode(tree));
 
-        for (tp, name, id) in indexer.store.read_tree(&tree)? {
+        for (tp, name, id) in store.read_tree(&tree)? {
             println!("  {} {} {:?}", hex::encode(id), name, tp);
         }
     }
